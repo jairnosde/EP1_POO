047b51b7c2e8da156fa586c998580a29d23a0cb1
jairnosde <49086434+jairnosde@users.noreply.github.com>
2019-10-19 09:57:07 -0500

    primer commit

---
+++ src/ep1_poo_o2019_fraga_edson_fceo170356_ocampo_karen_okgo171932/EP1_POO_O2019_Fraga_Edson_FCEO170356_Ocampo_Karen_OKGO171932.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ep1_poo_o2019_fraga_edson_fceo170356_ocampo_karen_okgo171932;
+
+/**
+ *
+ * @author Fraga
+ */
+public class EP1_POO_O2019_Fraga_Edson_FCEO170356_Ocampo_Karen_OKGO171932 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
