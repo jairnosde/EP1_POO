11e81935f048b6528167b64bc4d869cd71f9c889
jairnosde <49086434+jairnosde@users.noreply.github.com>
2019-10-19 12:26:43 -0500

    prueba 3

--- src/ep1_poo_o2019_fraga_edson_fceo170356_ocampo_karen_okgo171932/EP1_POO_O2019_Fraga_Edson_FCEO170356_Ocampo_Karen_OKGO171932.java
+++ src/ep1_poo_o2019_fraga_edson_fceo170356_ocampo_karen_okgo171932/EP1_POO_O2019_Fraga_Edson_FCEO170356_Ocampo_Karen_OKGO171932.java
@@ -5,6 +5,8 @@
  */
 package ep1_poo_o2019_fraga_edson_fceo170356_ocampo_karen_okgo171932;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Fraga
@@ -16,7 +18,34 @@ public class EP1_POO_O2019_Fraga_Edson_FCEO170356_Ocampo_Karen_OKGO171932 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundo");
+      Scanner sc = new Scanner(System.in);
+        
+        
+        int opc;
+        do{
+            System.out.println("EP1 POO\n");
+            System.out.println("1: Parte I");
+            
+            opc = sc.nextInt();
+            switch(opc){
+                case 1:
+                    
+                break;
+                case 2:
+                    //mapa.menu();
+                break;
+                case 3:
+                break;
+                case 4:
+                    //coleccion.menu();
+                break;
+                default:
+                break;
+            }
+        }while(opc != 0);
+    }
+        
+        
     }
     
-}
+
